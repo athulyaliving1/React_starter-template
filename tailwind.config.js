82fd5c0c6d9e5943e78894bd68c98ec22fbe5228
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2022-09-20 14:11:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      ss: "320px",
+      // => @media (min-width: 640px) { ... }
+
+      sm: "375px",
+      sl: "425px",
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+      Roboto: ["Roboto", "sans-serif"],
+      sans: ["Open Sans", "sans-serif"],
+      gill: ["Roboto", "sans-serif"],
+    },
+
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
   },
   plugins: [],
-}
+};
